b7466f2b1a59776ac2afa88739fb76ec58577a2d
surya <suryamanthena2016@gmail.com>
2021-04-10 10:38:57 +0530

    hello sir

---
+++ Hello/src/My1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class My1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
---
+++ Hello/src/testpack/Simple.java
@@ -0,0 +1,10 @@
+package testpack;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
